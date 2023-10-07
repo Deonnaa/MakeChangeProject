d714c2a91b502d69ce5221effd7a8b34fd45366e
Deonnaa <aponte.deonna@gmail.com>
2023-10-07 17:23:20 -0400

    complete register

--- src/makechange/Register.java
+++ src/makechange/Register.java
@@ -3,21 +3,77 @@ package makechange;
 public class Register {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		java.util.Scanner scanner = new java.util.Scanner(System.in);
 
+		System.out.print("Enter Purchase Price: $");
+		double purchasePrice = scanner.nextDouble();
+
+		System.out.print("Enter Amount Tendered: $");
+		double amountTendered = scanner.nextDouble();
+
+		double change = amountTendered - purchasePrice;
+		change += .0005;
+
+		// Prints out change <0, >0, =0
+		if (change < 0) {
+			System.err.println("You are short: $" + ((amountTendered - purchasePrice) * -1)); // Payment too low
+		} else if (change == 0) {
+			System.out.println("You are owed: $0.00"); // Exact change output
+		} else {
+			System.out.println("You are owed: $" + change); // Bills and cents output
+
+			int numOfDenom = 0;
+
+			// Prints out bills
+			if (change / 20 >= 1) {
+				numOfDenom = (int) (change / 20);
+				change = change % 20;
+				System.out.print("Twenties: " + numOfDenom + ", ");
+			}
+			if (change / 10 >= 1) {
+				numOfDenom = (int) (change / 10);
+				change = change % 10;
+				System.out.print("Tens: " + numOfDenom + ", ");
+			}
+			if (change / 5 >= 1) {
+				numOfDenom = (int) (change / 5);
+				change = change % 5;
+				System.out.print("Fives: " + numOfDenom + ", ");
+			}
+			if (change / 1 >= 1) {
+				numOfDenom = (int) (change / 1);
+				change = change % 1;
+				System.out.print("Ones: " + numOfDenom + ", ");
+			}
+
+			// Prints out cents
+			if (change / .25 >= 1) {
+				numOfDenom = (int) (change / .25);
+				change = change % .25;
+				System.out.print("Quarters: " + numOfDenom + ", ");
+			}
+			if (change / .10 >= 1) {
+				numOfDenom = (int) (change / .10);
+				change = change % .10;
+				System.out.print("Dimes: " + numOfDenom + ", ");
+			}
+			if (change / .05 >= 1) {
+				numOfDenom = (int) (change / .05);
+				change = change % .05;
+				System.out.print("Nickles: " + numOfDenom + ", ");
+			}
+			if (change / .01 >= 1) {
+				numOfDenom = (int) (change / .01);
+				change = change % .01;
+				System.out.print("Pennies: " + numOfDenom);
+			}
+
+		}
+		scanner.close();
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
+
 //In the cash register we will calculate the amount of change returned to a customer based on the 
 //purchase price and the amount tendered. We will also notify the attendant how many of each type of 
 //currency ($20 ,$10 ,$5 ,$1, .25c, .10c, .05c, .01c) is needed to make the change for the customer.
@@ -40,3 +96,13 @@ public class Register {
 //User Story #4
 //If the amount tendered is more than the cost of the item, display the number of bills and coins that 
 //should be given to the customer. Denominations that are not used will not be displayed.
+
+//Here are example test conditions:
+//
+//Amount: .67, Tendered: .50, Result: Error message
+//Amount: .67, Tendered: 1.00, Result: 1 quarter, 1 nickel, 3 pennies.
+//Amount: .59, Tendered: 1.00, Result: 1 quarter, 1 dime, 1 nickel, 1 penny.
+//Amount: 3.96, Tendered: 20.00, Result: 1 ten dollar bill, 1 five dollar bill, 1 one dollar bill, 
+//4 pennies.
+//Amount: any amount less than 20.00, Tendered: anything greater than amount: correct denominations 
+//for correct change.
